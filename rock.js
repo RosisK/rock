1ba8b16923ef48932589194006661628135094f4
RosisK <rosiskharel5@gmail.com>
2025-06-08 20:34:39 +0545

    Update JS

--- rock.js
+++ rock.js
@@ -1,3 +1,7 @@
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
+
+const result = document.querySelector('.result');
+const moves = document.querySelector('.moves');
+const score = document.querySelector('.score');
